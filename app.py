da52a362a3bcc47d9d0382cb1b411568e4f1a4c3
sunilmsuni596-dotcom <sunilmsuni596@gmail.com>
2026-05-06 13:39:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ if st.button("Generate QR"):
     img = Image.open("qr.png")
     st.image(img, caption= "Generated QR Code")
     with open("qr.png","rb") as f:
-      st.download_button("Download QR", f,file_name="qr.png)
+      st.download_button("Download QR", f,file_name="qr.png")
   else:
       st.warning("Please enter some text")
